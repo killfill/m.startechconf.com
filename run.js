0e690975a9ef6c13709228d51f96746c81addc99
KillFill <pneumann@gmail.com>
2011-06-12 20:51:24 -0400

    ...

---
+++ run.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var cluster = require('cluster')
+  , live = require('cluster-live')
+  , mail = require('cluster-mail');
+
+cluster('./lib/app.js')
+  .set('workers', 3)
+  .use(cluster.stats({connections: true, lightRequests: true}))
+  .use(cluster.logger('logs'))
+  .use(cluster.pidfiles('pids'))
+  .use(live())
+  .use(mail('pneumann@gmail.com', {from: 'node@sofsis.cl'}))
+  .listen(3001);
